c59b35e25b1a916b1f08ee3534ef5e0d18bdc080
Ankit Mehra <110310312+MehraAnkit@users.noreply.github.com>
2023-10-31 19:55:10 +0530

    Update TopologicalSort.cpp

--- TopologicalSort.cpp
+++ TopologicalSort.cpp
@@ -1,3 +1,4 @@
+//DFS CODE
 class Solution
 {
     private:
@@ -25,6 +26,48 @@ class Solution
 	    while(!st.empty()){
 	        sol.push_back(st.top());
 	        st.pop();
+	    }
+	    return sol;
+	}
+};
+
+
+
+//BFS CODE
+
+
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> vis(V, 0),sol;
+	    vector<int> ind(V, 0);
+	    
+	    for(int i=0;i<V;i++){
+	        for(int j=0;j<adj[i].size();j++){
+	            ind[adj[i][j]]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0;i<V;i++){
+	        if(ind[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        sol.push_back(node);
+	        for(auto ele: adj[node]){
+	            ind[ele]--;
+	            if(ind[ele]==0){
+	                q.push(ele);
+	            }
+	        }
 	    }
 	    return sol;
 	}
