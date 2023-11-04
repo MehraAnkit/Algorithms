9b885f6c63c8839f924a992882928ca3c06e4c5f
Ankit Mehra <110310312+MehraAnkit@users.noreply.github.com>
2023-11-04 18:32:24 +0530

    Create levelOrder1.cpp

---
+++ Codes/BinaryTree/levelOrder1.cpp
@@ -0,0 +1,40 @@
+* A binary tree Node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    {
+      //Your code here
+      queue<Node*> q;
+      q.push(node);
+      vector<int> sol;
+      while(!q.empty()){
+          Node* cur=q.front();
+          q.pop();
+          sol.push_back(cur->data);
+          if(cur->left!=NULL){
+              q.push(cur->left);
+          }
+          if(cur->right!=NULL){
+              q.push(cur->right);
+          }
+      }
+      return sol;
+    }
+};
