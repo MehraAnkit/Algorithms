9f994880d9860529d0c70f2ddaa56f0689bc1eef
MehraAnkit <110310312+MehraAnkit@users.noreply.github.com>
2023-11-11 10:44:33 +0530

    Update LCA.cpp

--- Codes/BinaryTree/LCA.cpp
+++ Codes/BinaryTree/LCA.cpp
@@ -1,19 +1,38 @@
-class Solution{
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{
     public:
-        Node* LCA(Node *root, int n1, int n2)
-        {
-            // code here
-            if(root==NULL){
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       if(root==NULL){
            return NULL;
        }
-       int cur=root->data;
-       if(cur< n1 and cur<n2){
-           return LCA(root->right, n1, n2);
+       if(root->data==n1 || root->data==n2){
+           return root;
        }
-       if(cur> n1 and cur>n2){
-           return LCA(root->left, n1, n2);
+       Node* lca1=lca(root->left, n1, n2);
+       Node* lca2=lca(root->right, n1, n2);
+       if(lca1==NULL){
+           return lca2;
+       }
+       if(lca2==NULL){
+           return lca1;
        }
        return root;
-        }
-
+    }
 };
